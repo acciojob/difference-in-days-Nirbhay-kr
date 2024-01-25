075c98d2b3c439fdeced4cce06ec3b4d55320a30
Nirbhay kumar <126154810+Nirbhay-kr@users.noreply.github.com>
2024-01-25 11:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	if(date1 == date2){
+		console.log(0);
+	}else if(date1 > date2){
+		console.log(date1 - date2);
+	}else{
+		console.log(date2 - date1);
+	}
 };
 
 // Do not change the code below.
