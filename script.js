d71a0918a5a7a7698a737f636eb4d34850be99a8
Nirbhay kumar <126154810+Nirbhay-kr@users.noreply.github.com>
2024-01-25 11:22:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
 	if(date1 == date2){
-		console.log(0);
+		return (0);
 	}else if(date1 > date2){
-		console.log(date1 - date2);
+		return (date1 - date2);
 	}else{
-		console.log(date2 - date1);
+		return (date2 - date1);
 	}
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
+//const dateOne = prompt("Enter Start Date.");
+//const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
